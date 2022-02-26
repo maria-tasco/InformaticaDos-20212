602a785d124c1309585dcb59a128d510ed95e840
“Your <maria.tasco@udea.edu.co>
2022-02-26 13:56:38 -0500

    se sube el codigo de qt para la lectura del archivo aun falta la escritura en forma de arreglo y se sube los cambios del programa tinkercad con los arduinos conectados como esclavo y maestro (no hay codigo concreto) a avance en el formato del informe con la referencia 1 como link del proyecto en tinkercad

--- Parcial1/Codigos/QT/Parcial1/main.cpp
+++ Parcial1/Codigos/QT/Parcial1/main.cpp
@@ -2,6 +2,12 @@
 
 int main()
 {
-
+    char archivo_text[]="mensajeInicial.txt", archivo_array[]="mensajeFinal.txt";
+    char *data = nullptr;
+    unsigned long long tamArchivo = 0;
+    tamArchivo = tam(archivo_text);
+    data = new char[tamArchivo + 1];
+    leer(archivo_text, tamArchivo,data);
+    //leí y toda mi información la tengo en data
     return 0;
 }
--- Parcial1/Codigos/QT/Parcial1/manejo_archivo.h
+++ Parcial1/Codigos/QT/Parcial1/manejo_archivo.h
@@ -1,11 +1,14 @@
 #ifndef MANEJO_ARCHIVO_H
 #define MANEJO_ARCHIVO_H
 
+#include <fstream>
+#include <iostream>
+#include <string.h>
+using namespace std;
 
-class manejo_archivo
-{
-public:
-    manejo_archivo();
-};
+void crear (char *nombre);
+void escribir(char *nombre, char *datos, unsigned long long int tam, bool tipo);
+unsigned long long tam(char *nombre);
+bool leer (char *nombre, unsigned long long tam,char *data);
 
 #endif // MANEJO_ARCHIVO_H
